7cd562ec84a7a50af87a5edd2bf035dad151c25a
Van <monsterMU90@gmail.com>
2023-09-28 22:53:32 -0700

    adding new rock-paper-scissor game

--- myscript.js
+++ myscript.js
@@ -3,55 +3,105 @@ function getComputerChoice() {
     return (arr[Math.floor(Math.random() * arr.length)]);
 }
 
+function disableButtons() {
+    btn.forEach(button2 => {
+        button2.disabled = true;
+    });
+}
+const btn = document.querySelectorAll("#btn");
+const para = document.querySelector("#result");
+const computerUI = document.querySelector("#computer-ui");
+const playerUI = document.querySelector("#player-ui");
 
 
-function playRound(playerSelection, computerSelection) {
-    const userChoice = playerSelection.toUpperCase();
+btn.forEach(button => {
+    button.addEventListener("click", () => {
+        playRound(button.value);
+        playerUI.textContent = button.value;
 
+        if (button.value === "ROCK") {
+            playerUI.style.borderColor = "rgb(196, 166, 224)";
+        }
+        else if (button.value === "PAPER") {
+            playerUI.style.borderColor = "rgb(152, 224, 152)";
+        }
+        else {
+            playerUI.style.borderColor = "rgb(241, 164, 164)";
+        }
+        
+    })
+});
+
+function playRound(userChoice) {
+    const computerSelection = getComputerChoice();
+    computerUI.textContent = computerSelection;
 
     if (userChoice === "ROCK") {
         if (computerSelection === "ROCK") {
-            console.log("You Tied! Rock matches Rock");
+            para.textContent = "You Tied! Rock matches Rock";
+            computerUI.style.borderColor = "rgb(196, 166, 224)";
+            para.style.color = "rgb(112, 112, 112)";
         }
 
         else if (computerSelection === "PAPER") {
-            console.log("You Lose! Paper beats Rock");
+            para.textContent = "You Lose! Paper beats Rock";
+            computerUI.style.borderColor = "rgb(152, 224, 152)";
+            para.style.color = "rgb(211, 133, 133)";
         }
 
         else {
-            console.log("You Win! Rock beats Scissor");
+            para.textContent = "You Win! Rock beats Scissor";
+            computerUI.style.borderColor = "rgb(241, 164, 164)";
+            para.style.color = "rgb(123, 211, 145)";
         }
     }
 
 
     if (userChoice === "SCISSOR") {
         if (computerSelection === "ROCK") {
-            console.log("You Lose! Rock beats Scissor");
+            para.textContent = "You Lose! Rock beats Scissor";
+            computerUI.style.borderColor = "rgb(196, 166, 224)";
+            para.style.color = "rgb(211, 133, 133)";
         }
 
         else if (computerSelection === "SCISSOR") {
-            console.log("You Tied! Scissor matches Scissor");
+            para.textContent = "You Tied! Scissor matches Scissor";
+            computerUI.style.borderColor = "rgb(241, 164, 164)";
+            para.style.color = "rgb(112, 112, 112)";
+            
         }
 
         else {
-            console.log("You Win! Scissor beats Paper");
+            para.textContent = "You Win! Scissor beats Paper";
+            computerUI.style.borderColor = "rgb(152, 224, 152)";
+            para.style.color = "rgb(123, 211, 145)";
         }
     }   
 
     if (userChoice === "PAPER") {
         if (computerSelection === "ROCK") {
-            console.log("You Win! Paper beats Rock");
+            para.textContent = "You Win! Paper beats Rock";
+            computerUI.style.borderColor = "rgb(196, 166, 224)";
+            para.style.color = "rgb(123, 211, 145)";
         }
 
         else if (computerSelection === "SCISSOR") {
-            console.log("You Lose! Scissor beats Paper");
+            para.textContent = "You Lose! Scissor beats Paper";
+            computerUI.style.borderColor = "rgb(241, 164, 164)";
+            para.style.color = "rgb(211, 133, 133)";
+            
         }
         else {
-            console.log("You Tied! Paper matches Paper");
+            para.textContent = "You Tied! Paper matches Paper";
+            computerUI.style.borderColor = "rgb(152, 224, 152)";
+            para.style.color = "rgb(112, 112, 112)";
         }
     }
+    
+        
+};
 
-}
+
 
 
 function game(number) {
@@ -65,7 +115,7 @@ function game(number) {
     }
 }
 
-console.log(game(5));
+
 
 
 
